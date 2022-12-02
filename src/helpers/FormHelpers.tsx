f62b95566ff296526fb27eca0eccff4a8988b1ba
AliNemir <alinemir283@gmail.com>
2022-12-02 22:21:55 +0200

    feat: adding the full page form

--- src/helpers/FormHelpers.tsx
+++ src/helpers/FormHelpers.tsx
@@ -1,3 +1,18 @@
-function ErrorMessage({ message }: any) {
+export function ErrorMessage({ message }: any) {
     return <label className='text-sm font-semibold text-red-500' >{message}</label>;
 }
+
+export const isMobileNumber = (e: any) => {
+    if (!e) {
+        return true;
+    }
+    return /([0-9]●?){9,14}[0-9]$/
+    .test(e);
+};
+export const isPasswordValid = (e: any) => {
+    if (!e) {
+        return true;
+    }
+    return /^[a-zA-Z0-9]{8,16}$/
+    .test(e);
+};
